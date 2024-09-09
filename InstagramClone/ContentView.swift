fa079ceeba590e9839faa164f7a4fa2231fbf2f1
sangwook16074 <qazws78941@gmail.com>
2024-09-10 08:42:31 +0900

    Initial Commit

---
+++ InstagramClone/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  InstagramClone
+//
+//  Created by 한상욱 on 9/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        AppView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by 한상욱 on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
